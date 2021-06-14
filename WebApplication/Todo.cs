c3b81e8ade33d1b95e10d12aba98cea1db79e503
Gøran Humlestøl <goransh@live.no>
2021-06-14 14:40:30 +0200

    Add web app with async and json examples

---
+++ WebApplication/Todo.cs
@@ -0,0 +1,13 @@
+﻿namespace WebApplication {
+    /*
+     * C# 9 introduced records which are classes that automatically implement
+     * Equals and HashCode. They are immutable by default and have a simplified syntax
+     * for defining properties and constructors.
+     */
+    public record Todo(
+        int Id,
+        int UserId,
+        string Title,
+        bool Completed = false
+    );
+}
---
+++ WebApplication/TodoController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Immutable;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace WebApplication {
+    [Controller]
+    public class TodoController {
+        private readonly ILogger<TodoController> _logger;
+        private readonly TodoRepository _todoRepository;
+
+        public TodoController(ILogger<TodoController> logger, TodoRepository todoRepository) {
+            // dependency injection
+            _logger = logger;
+            _todoRepository = todoRepository;
+        }
+
+        [Route("/todos")]
+        public async Task<IActionResult> ListTodos([FromQuery(Name = "completed")] bool completed = false) {
+            _logger.LogInformation("Fetching Todos");
+            var todos = await _todoRepository.ListTodos();
+
+            // structured logging
+            _logger.LogInformation("Received {NumberOfTodos} todos", todos.Count);
+
+            var filtered = todos
+                .Where(todo => todo.Completed == completed)
+                .ToImmutableList();
+
+            _logger.LogInformation("Will return {NumberOfFilteredTodos} todos", filtered.Count);
+
+            return new OkObjectResult(filtered);
+        }
+    }
+}
